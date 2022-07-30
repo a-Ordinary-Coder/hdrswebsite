7e846b71541db9160791c6fbd1944b67a8b2a2cd
LvJunjie <2397561692@qq.com>
2022-07-30 18:08:59 +0800

    添加轮播图

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,7 @@ import React, { FC } from 'react'
 import {
     Link,
     Routes,
-    Route,
-    useNavigate,
+    Route
 } from 'react-router-dom'
 
 // 导入组件
@@ -11,19 +10,27 @@ import Index from '../HomeSub/Index'
 import Job from '../HomeSub/Job'
 import Ptjob from '../HomeSub/Ptjob'
 
+import sty from './sty.less'
+
+const styJons = { "color": "green" };
 export default class Home extends React.Component {
+    
     render(): React.ReactNode {
         return (
             <div>
-                <div>
-                    <ul style={{listStyleType:'none'}}>
-                        <li><Link to="/index">首页</Link></li>
-                        <li><Link to="/job">集市</Link></li>
-                        <li><Link to="/ptjob">兼职</Link></li>
+                <div style={{ width: '100%', height: '15px' }}>
+                    <ul  style={{ listStyleType: 'none', lineHeight: '15px', marginLeft: '26%' }}>
+                        <li style={{ display: 'inline-block', float: 'left', width: '16%' }}>
+                            <Link to="/index" style={styJons}>首页</Link></li>
+                        <li style={{ display: 'inline-block', float: 'left', width: '16%' }}>
+                            <Link to="/job"
+                             >集市</Link></li>
+                        <li style={{ display: 'inline-block', float: 'left', width: '16%' }}>
+                            <Link to="/ptjob">兼职</Link></li>
                     </ul>
-
                 </div>
-                <div>
+
+                <div style={{ marginTop: '0px', display: "block" }}>
                     <Routes>
                         <Route path="/index" element={<Index />}></Route>
                         <Route path="/" element={<Index />}></Route>
--- src/pages/HomeSub/Index/index.tsx
+++ src/pages/HomeSub/Index/index.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 
-export default class Index extends React.Component {
-    render() {
-        return <div>Index页面</div>
-    }
+// 导入antd-mobile组件
+import { Button, Space, Swiper, Toast } from 'antd-mobile'
+import { SwiperRef } from 'antd-mobile/es/components/swiper'
+
+import styles from './sty.less'
+
+const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac']
+
+const items = colors.map((color, index) => (
+    <Swiper.Item key={index} >
+        <div
+            className={styles.content}
+            style={{ background: color,height:'120px'}}
+            onClick={() => {
+                Toast.show(`你点击了卡片 ${index + 1}`)
+            }}
+        >
+            {index + 1}
+        </div>
+    </Swiper.Item>
+))
+
+export default () => {
+    const ref = useRef<SwiperRef>(null)
+    return (
+        <div>
+
+            <Swiper autoplay loop>{items}</Swiper>
+
+        </div>
+
+    )
 }
