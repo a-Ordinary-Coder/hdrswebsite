04dde86ae58d13500d6ca59354e2a18322473c11
LvJunjie <2397561692@qq.com>
2022-08-03 00:05:13 +0800

    md

--- src/pages/HomeSub/Index/index.tsx
+++ src/pages/HomeSub/Index/index.tsx
@@ -3,18 +3,28 @@ import React, { useRef } from "react";
 // 导入antd-mobile组件
 import { Button, Space, Swiper, Toast } from 'antd-mobile'
 import { SwiperRef } from 'antd-mobile/es/components/swiper'
+import axios from 'axios'
 
 import styles from './sty.less'
+import { url } from "inspector";
+
+
 
 const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac']
+const bgImgs = ['/assets/imgs/3.jpg', './imgs/4.jpg', './imgs/5.jpg', './imgs/2.jpg']
 
-const items = colors.map((color, index) => (
+const items = bgImgs.map((bgImg, index) => (
     <Swiper.Item key={index} >
         <div
             className={styles.content}
-            style={{ background: color,height:'120px'}}
+            style={{
+                // todo: 图片需要网络图片
+                backgroundImage: 'url(' + 'file:///H:/HuaDa/RSSystem/WebSite/hdrswebsite/src/assets/imgs/3.jpg' + ')'
+                // backgroundImage: bgImg
+            }}
             onClick={() => {
                 Toast.show(`你点击了卡片 ${index + 1}`)
+                // todo: 跳转
             }}
         >
             {index + 1}
@@ -33,3 +43,14 @@ export default () => {
 
     )
 }
+
+
+// export default class Index extends React.Component {
+//     render(): React.ReactNode {
+//         return (
+//             <div>
+//                 <Swiper autoplay loop>{items}</Swiper>
+//             </div>
+//         )
+//     }
+// }
